89aac654abc6e7d47560ab7c3951e3a577c6de8b
Victor Donizete <victordonizete65@gmail.com>
2024-10-16 00:40:41 -0300

    adicionando imagens e php da secao links

--- pages/home/components/links.php
+++ pages/home/components/links.php
@@ -1,24 +1,33 @@
 <div class="mt-28">
+    <?php 
+        $social_media = [
+            [
+                'image' => '../../../assets/LinkedinLogo.svg',
+                'name' => 'Linkedin',
+            ],
+            [
+                'image' => '../../../assets/InstagramLogo.svg',
+                'name' => 'Instagram',
+            ],
+            [
+                'image' => '../../../assets/GithubLogo.svg',
+                'name' => 'Github',
+            ],
+            [
+                'image' => '../../../assets/EnvelopeSimple.svg',
+                'name' => 'E-mail',
+            ],            
+        ]
+    ?>
     <p class="text-center font-mono text-violet-700 text-lg">Contato</p>
     <h3 class="text-center text-2xl font-bold text-white">Gostou do meu trabalho?</h3>
     <p class="text-center text-slate-500">Entre em contato ou acompanhe as minhas redes sociais!</p>
     <div class="cards flex flex-col items-center mt-8">
-        <div class="my-2 card flex bg-gray-800 w-96 rounded-lg h-16 justify-between py-0 px-3 items-center">
-            <i class="flex text-center gap-4 text-white items-center font-medium"><img src="../../../assets/LinkedinLogo.svg" alt=""> Linkedin</i>
-            <i><img src="../../../assets/ArrowUpRight.svg" alt=""></i>
-        </div>
-        <div class="my-2 card flex bg-gray-800 w-96 rounded-lg h-16 justify-between py-0 px-3 items-center">
-            <i class="flex text-center gap-4 text-white items-center font-medium"><img src="../../../assets/LinkedinLogo.svg" alt=""> Linkedin</i>
-            <i><img src="../../../assets/ArrowUpRight.svg" alt=""></i>
-        </div>
-        <div class="my-2 card flex bg-gray-800 w-96 rounded-lg h-16 justify-between py-0 px-3 items-center">
-            <i class="flex text-center gap-4 text-white items-center font-medium"><img src="../../../assets/LinkedinLogo.svg" alt=""> Linkedin</i>
-            <i><img src="../../../assets/ArrowUpRight.svg" alt=""></i>
-        </div>
-        <div class="my-2 card flex bg-gray-800 w-96 rounded-lg h-16 justify-between py-0 px-3 items-center">
-            <i class="flex text-center gap-4 text-white items-center font-medium"><img src="../../../assets/LinkedinLogo.svg" alt=""> Linkedin</i>
-            <i><img src="../../../assets/ArrowUpRight.svg" alt=""></i>
-        </div>
-
+        <?php foreach($social_media as $media): ?>
+            <div class="group border-2 border-transparent hover:border-gray-500  my-2 card flex bg-gray-800 w-96 rounded-lg h-16 justify-between py-0 px-3 items-center transition-border duration-300 cursor-pointer">
+                <i class="group-hover:text-gray-500 flex text-center gap-4 text-white items-center font-medium transition-border duration-300"><img src=<?=$media['image']?> alt=""><?=$media['name']?></i>
+                <i><img src="../../../assets/ArrowUpRight.svg" alt=""></i>
+            </div>
+        <?php endforeach; ?>
     </div>
 </div>
